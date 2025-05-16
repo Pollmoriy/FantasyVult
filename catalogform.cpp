7c4789d75a53ff0d90ddcd543cfcab481457be28
polina <6704523472@my.lerna.by>
2025-05-16 18:22:33 +0300

    Добавлены все карточки

--- catalogform.cpp
+++ catalogform.cpp
@@ -19,17 +19,14 @@ QWidget* CatalogForm::createUniverseCard(const QString &name, const QString &ima
 {
     qDebug() << "Создание карточки с именем:" << name << " и изображением:" << imagePath;
 
-    // Карточка (основной виджет)
     QWidget *cardWidget = new QWidget(parent);
     cardWidget->setStyleSheet("background-color: transparent; border-radius: 20px;");
     cardWidget->setFixedSize(566, 372);
 
-    // Фрейм-контейнер для всей карточки
     QFrame *universeCard = new QFrame(cardWidget);
     universeCard->setStyleSheet("background-color: transparent;");
     universeCard->setFixedSize(566, 372);
 
-    // Метка изображения
     QLabel *imageLabel = new QLabel(universeCard);
     imageLabel->setFixedSize(566, 321);
     QPixmap pixmap(imagePath);
@@ -41,7 +38,6 @@ QWidget* CatalogForm::createUniverseCard(const QString &name, const QString &ima
     imageLabel->setStyleSheet("border: none;");
     imageLabel->setGeometry(0, 0, 566, 321);
 
-    // Кнопка лайка
     QPushButton *likeButton = new QPushButton(universeCard);
     likeButton->setIcon(QIcon(":/symbols/heart_white.svg"));
     likeButton->setIconSize(QSize(71, 67));
@@ -58,7 +54,6 @@ QWidget* CatalogForm::createUniverseCard(const QString &name, const QString &ima
         }
     });
 
-    // Информационный фрейм снизу
     QFrame *infoFrame = new QFrame(universeCard);
     infoFrame->setGeometry(0, 313, 566, 58);
     infoFrame->setStyleSheet(
@@ -67,7 +62,6 @@ QWidget* CatalogForm::createUniverseCard(const QString &name, const QString &ima
         "border-bottom-left-radius: 20px; border-bottom-right-radius: 20px;"
         );
 
-    // Метка названия
     QLabel *nameLabel = new QLabel(name, universeCard);
     int fontId = QFontDatabase::addApplicationFont(":/fonts/CinzelDecorative-Regular.ttf");
     QString family = QFontDatabase::applicationFontFamilies(fontId).at(0);
@@ -79,17 +73,17 @@ QWidget* CatalogForm::createUniverseCard(const QString &name, const QString &ima
     nameLabel->setWordWrap(true);
     nameLabel->setGeometry(0, 295, 566, 77);
 
-    // Добавляем карточку в контейнер rowContainer с компоновкой horizontalLayout_3
-    if (ui->horizontalLayout_3) {
-        ui->horizontalLayout_3->addWidget(cardWidget);
-        qDebug() << "Карточка добавлена в rowContainer!";
-    } else {
-        qDebug() << "Ошибка: horizontalLayout_3 не найден!";
-    }
+    // Убираем добавление сюда!
+    // if (ui->horizontalLayout_3) {
+    //    ui->horizontalLayout_3->addWidget(cardWidget);
+    //    qDebug() << "Карточка добавлена в rowContainer!";
+    // } else {
+    //    qDebug() << "Ошибка: horizontalLayout_3 не найден!";
+    // }
 
-    // Вернуть готовую карточку
     return cardWidget;
 }
+
 
 
 
@@ -112,25 +106,44 @@ CatalogForm::CatalogForm(QWidget *parent)
     QSqlDatabase db = QSqlDatabase::database(); // Получаем уже открытое соединение по умолчанию
 
     QSqlQuery query(db);
-    if (query.exec("SELECT name, main_image FROM Universe WHERE id_universe IN (1, 2)")) {
-        qDebug() << "Запрос выполнен успешно!";
-        while (query.next()) {
-            QString name = query.value(0).toString();
-            QString imagePath = query.value(1).toString();
-            qDebug() << "Обработка вселенной: " << name << " с изображением: " << imagePath;
-
-            // Создаём карточку с полученными данными
-            QWidget* card = createUniverseCard(name, imagePath, ui->cardContainer);
-            if (card) {
-                qDebug() << "Карточка успешно добавлена в контейнер.";
-            } else {
-                qDebug() << "Ошибка создания карточки!";
-            }
-        }
-    } else {
+    if (!query.exec("SELECT name, main_image FROM Universe WHERE id_universe IN (1,2,3,4,5,6,7,8,9,10,11,12,13,14) ORDER BY id_universe")) {
         qDebug() << "Ошибка выполнения запроса:" << query.lastError().text();
+        return;
     }
 
+    // Список горизонтальных лэйаутов - создайте 7 в .ui с именами: horizontalLayout_4, ..., horizontalLayout_10
+    QList<QHBoxLayout*> rows = {
+        ui->horizontalLayout_3,
+        ui->horizontalLayout_4,
+        ui->horizontalLayout_5,
+        ui->horizontalLayout_6,
+        ui->horizontalLayout_7,
+        ui->horizontalLayout_8,
+        ui->horizontalLayout_9
+    };
+
+
+    int cardIndex = 0;
+    while (query.next()) {
+        QString name = query.value(0).toString();
+        QString imagePath = query.value(1).toString();
+
+        QWidget* card = createUniverseCard(name, imagePath, ui->cardContainer);
+        if (!card) {
+            qDebug() << "Ошибка создания карточки для" << name;
+            continue;
+        }
+
+        int rowNum = cardIndex / 2;
+        if (rowNum >= rows.size()) {
+            qDebug() << "Превышено количество рядов!";
+            break;
+        }
+
+        rows[rowNum]->addWidget(card);
+
+        ++cardIndex;
+    }
 
 
 
